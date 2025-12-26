f9fec4aae44dccc355094a3c02eae26c38d58c90
{Dalzero} <{lunawdal10@gmail.com}>
2025-12-26 16:01:22 +0900

    ?

--- 03UI/nhn/src/main/java/com/example/ex20.java
+++ 03UI/nhn/src/main/java/com/example/ex20.java
@@ -3,23 +3,22 @@ public class ex20 {
     public static void main(String[] args) {
         int a = 10, b = 3;
         String op = "/";
-        int result = 0;
-        switch (op) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
+        int result = switch (op) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> {
+                if (b == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    yield 0;
+                }
+                yield a / b;
+            }
+            default -> {
                 System.out.println("잘못된 연산자");
-        }
+                yield 0;
+            }
+        };
         System.out.println("result: " + result); // 3
     }
 }
